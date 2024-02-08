1f5c3ad4d6378a9454b0745e66651d2e351b3675
alibhs <alibahsi772@gmail.com>
2024-02-08 17:45:07 +0300

    Ürünlere Resim Ekleme Operasyonu

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -10,6 +10,8 @@ import {
   Position,
 } from '../../../../services/admin/alertify.service';
 import { MatPaginator } from '@angular/material/paginator';
+import { DialogService } from '../../../../services/common/dialog.service';
+import { SelectProductImageDialogComponent } from '../../../../dialogs/select-product-image-dialog/select-product-image-dialog.component';
 
 declare var $:any;
 
@@ -22,7 +24,8 @@ export class ListComponent extends BaseComponent implements OnInit {
   constructor(
     spinner: NgxSpinnerService,
     private productService: ProductService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private dialogService:DialogService
   ) {
     super(spinner);
   }
@@ -33,6 +36,7 @@ export class ListComponent extends BaseComponent implements OnInit {
     'price',
     'createdDate',
     'updatedDate',
+    'photos',
     'edit',
     'delete',
   ];
@@ -60,6 +64,17 @@ export class ListComponent extends BaseComponent implements OnInit {
   //   const img : HTMLImageElement = event.srcElement;
   //   $(img.parentElement.parentElement).fadeOut(2000);
   // }
+
+  addProductImages(id:string){
+    this.dialogService.openDialog({
+      componentType: SelectProductImageDialogComponent,
+      data:id,
+      options:{
+        width:"1400px"
+      }
+
+    })
+  }
 
   async pageChanged(){
     await this.getProducts();
--- src/app/services/common/file-upload/file-upload.component.ts
+++ src/app/services/common/file-upload/file-upload.component.ts
@@ -7,6 +7,8 @@ import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui
 import { MatDialog } from '@angular/material/dialog';
 import { FileUploadDialogComponent, FileUploadDialogState } from '../../../dialogs/file-upload-dialog/file-upload-dialog.component';
 import { DialogService } from '../dialog.service';
+import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from '../../../base/base.component';
 
 
 @Component({
@@ -19,7 +21,8 @@ export class FileUploadComponent {
     private alertifyService: AlertifyService,
     private customToastrService: CustomToastrService,
     private dialogService: DialogService,
-    private dialog: MatDialog) { }
+    private dialog: MatDialog,
+    private spinner: NgxSpinnerService) { }
 
   public files: NgxFileDropEntry[];
 
@@ -39,6 +42,7 @@ export class FileUploadComponent {
       componentType: FileUploadDialogComponent,
       data: FileUploadDialogState.Yes,
       afterClosed: () => {
+        this.spinner.show(SpinnerType.BallAtom)
 
         this.httpclientService.post({
           controller: this.options.controller,
@@ -48,6 +52,8 @@ export class FileUploadComponent {
         }, fileData).subscribe(data => {
 
           const successMessage: string = "Dosyalar başarıyla yüklenmiştir.";
+          this.spinner.hide(SpinnerType.BallAtom)
+
 
           if (this.options.isAdminPage) {
             this.alertifyService.message(successMessage, {
@@ -61,9 +67,12 @@ export class FileUploadComponent {
               position: ToastrPosition.TopRight
             })
           }
-        }, (errorResponse: HttpErrorResponse) => {
+        }
+        , (errorResponse: HttpErrorResponse) => {
           if (this.options.isAdminPage) {
-            this.alertifyService.message("Dosyalar yüklenirken beklenmeyen bir hata ile karşılmıştır.", {
+            this.alertifyService.message("Dosyalar yüklenirken beklenmeyen bir hata ile karşılmıştır.",
+             {
+
               dismissOther: true,
               messageType: MessageType.Success,
               position: Position.TopRight
